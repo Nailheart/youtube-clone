ddb2e465aade4ce1b7343369835fd28ee6884cd9
Yaroslav Lebedenko <nailheart4@gmail.com>
2022-12-10 19:19:59 +0200

    app layout

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,25 +1,28 @@
-import { Routes, Route } from "react-router-dom";
-import { 
+import { Routes, Route } from 'react-router-dom';
+import { AppRoute } from 'common/enums/enums';
+import { Header, Sidebar } from 'components/common/common';
+import {
   ChannelDetail,
   Feed,
-  Navbar,
   SearchFeed,
   VideoDetail
-} from "components/components";
-import { AppRoute } from 'common/enums/enums';
+} from 'components/components';
+import styles from './styles.module.scss';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello YouTube</h1>
-      <Navbar />
+    <div className={styles.app}>
+      <Header />
+      <div className={styles.container}>
+        <Sidebar />
 
-      <Routes>
-        <Route path={AppRoute.ROOT} element={<Feed />} />
-        <Route path={AppRoute.VIDEO_ID} element={<VideoDetail />} />
-        <Route path={AppRoute.CHANNEL_ID} element={<ChannelDetail />} />
-        <Route path={AppRoute.SEARCH_TERM} element={<SearchFeed />} />
-      </Routes>
+        <Routes>
+          <Route path={AppRoute.ROOT} element={<Feed />} />
+          <Route path={AppRoute.VIDEO_ID} element={<VideoDetail />} />
+          <Route path={AppRoute.CHANNEL_ID} element={<ChannelDetail />} />
+          <Route path={AppRoute.SEARCH_TERM} element={<SearchFeed />} />
+        </Routes>
+      </div>
     </div>
   );
 };
---
+++ src/components/common/sidebar/sidebar.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+const Sidebar = () => {
+  return (
+    <aside className={styles.sidebar}>Sidebar</aside>
+  );
+};
+
+export { Sidebar };
