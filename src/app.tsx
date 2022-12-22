70545e9282f46ca3b6dd69a23906f14acbe8477e
Yaroslav Lebedenko <nailheart4@gmail.com>
2022-12-22 15:20:28 +0200

    search feed loader

--- src/app.tsx
+++ src/app.tsx
@@ -8,13 +8,14 @@ import {
   VideoDetail
 } from 'components/components';
 import { feedLoader } from 'components/feed/feed';
+import { searchFeedLoader } from './components/search-feed/search-feed';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path={AppRoute.ROOT} element={<Layout />}>
     <Route index element={<Feed />} loader={feedLoader} />
     <Route path={AppRoute.VIDEO_ID} element={<VideoDetail />} />
     <Route path={AppRoute.CHANNEL_ID} element={<ChannelDetail />} />
-    <Route path={AppRoute.SEARCH_TERM} element={<SearchFeed />} />
+    <Route path={AppRoute.SEARCH_TERM} element={<SearchFeed />} loader={searchFeedLoader} />
   </Route>  
 ));
 
--- src/components/search-feed/search-feed.tsx
+++ src/components/search-feed/search-feed.tsx
@@ -1,7 +1,32 @@
+import { LoaderFunctionArgs, useLoaderData } from 'react-router-dom';
+import { PlaylistVideoResponseDto, PlaylistVideosResponseDto } from 'common/types/types';
+import { fetchFromAPI } from 'helpers/helpers';
+import { VideoCard } from 'components/common/common';
+import styles from './styles.module.scss';
+
 const SearchFeed = () => {
+  const videos = useLoaderData() as PlaylistVideosResponseDto;
+
   return (
-    <div>SearchFeed</div>
+    <div className={styles.searchFeed}>
+      {videos.items.map((item: PlaylistVideoResponseDto) => (
+        <VideoCard
+          key={item.id.videoId}
+          videoId={item.id.videoId}
+          channelId={item.snippet.channelId}
+          img={item.snippet.thumbnails.high.url}
+          title={item.snippet.title}
+          channelTitle={item.snippet.channelTitle}
+        />
+      ))}
+    </div>
   );
 };
 
-export { SearchFeed };
+const searchFeedLoader = async ({ params }: LoaderFunctionArgs) => {
+  const data = await fetchFromAPI(`search?q=${params.searchTerm}&part=snippet&regionCode=US&maxResults=50&order=date`);
+
+  return data;
+}
+
+export { SearchFeed, searchFeedLoader };
