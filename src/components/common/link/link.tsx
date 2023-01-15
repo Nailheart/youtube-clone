3b0da079e7b6200610ade294a8edbfdb402ed176
Yaroslav Lebedenko <nailheart4@gmail.com>
2023-01-15 17:31:16 +0200

    added default linl props

--- src/components/common/link/link.tsx
+++ src/components/common/link/link.tsx
@@ -1,6 +1,6 @@
-import { FC, ReactNode } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, NavLinkProps } from 'react-router-dom';
 import { AppRoute } from 'common/enums/enums';
+import { FC, ReactNode } from 'common/types/types';
 
 type Props = {
   to: AppRoute;
@@ -8,8 +8,10 @@ type Props = {
   className?: string;
 };
 
-const Link: FC<Props> = ({ to, children, className }) => (
-  <NavLink to={to} className={className}>{children}</NavLink>
+type LinkProps = Props & Omit<NavLinkProps, keyof Props>;
+
+const Link: FC<LinkProps> = ({ to, children, className, ...otherProps }) => (
+  <NavLink to={to} className={className} {...otherProps}>{children}</NavLink>
 );
 
 export { Link };
