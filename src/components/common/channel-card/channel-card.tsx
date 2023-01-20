5bc92c3235a950d47f2e989247069faba78f1e1e
Yaroslav Lebedenko <nailheart4@gmail.com>
2023-01-20 20:27:03 +0200

    fix channelCard url

--- src/components/common/channel-card/channel-card.tsx
+++ src/components/common/channel-card/channel-card.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
+import { AppRoute } from 'common/enums/enums';
 import { FC } from 'common/types/types';
+import { insertUrlParams } from 'helpers/helpers';
 import styles from './styles.module.scss';
 
 type Props = {
@@ -11,10 +13,10 @@ type Props = {
 const ChannelCard: FC<Props> = ({ channelId, img, title }) => {
   return (
     <div className={styles.channelCard}>
-      <Link to={`/channel/${channelId}`}>
+      <Link to={insertUrlParams(AppRoute.CHANNEL_ID, {id: channelId})}>
         <img className={styles.img} src={img} alt={title} />
       </Link>
-      <Link className={styles.title} to={`/channel/${channelId}`}>
+      <Link className={styles.title} to={insertUrlParams(AppRoute.CHANNEL_ID, {id: channelId})}>
         {title}
       </Link>
     </div>
