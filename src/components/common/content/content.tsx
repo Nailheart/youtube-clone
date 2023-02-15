aabd083bfc5ab497dd0565cbc1073fae78110722
Yaroslav Lebedenko <nailheart4@gmail.com>
2023-02-15 12:00:21 +0200

    + linkify time

--- src/components/common/content/content.tsx
+++ src/components/common/content/content.tsx
@@ -1,26 +1,35 @@
 import 'linkify-plugin-hashtag';
+import 'linkify-plugin-keyword';
 import Linkify from 'linkify-react';
+import registerKeywords from 'linkify-plugin-keyword';
 import { FC } from 'common/types/types';
-import { sanitizeHTML } from 'helpers/helpers';
+import { getSeconds, sanitizeHTML } from 'helpers/helpers';
 
 type Props = {
   content: string;
   className?: string;
 };
 
+// TODO: need refactor
 const Content: FC<Props> = ({ content, className }) => {
   const replacedContent = content.replace(/<br\s*\/?>/gi,'\n'); // replace br tag
+  const regex = /(([0-9]|[0-9]{2}):[0-9]{2}:[0-9]{2})|(([0-9]|[0-9]{2}):[0-9]{2})/g; // time 😐
+  const timeList = replacedContent.match(regex);
+
+  registerKeywords(timeList ?? []);
 
   return (
     <div className={className}>
       <Linkify
         options={{
-          target: "_blank",
-          formatHref: function (href, type) {
-            if (type === "hashtag") {
-              href = "https://www.youtube.com/hashtag/" + href.substring(1);
-            }
-            return href;
+          rel: 'nofollow noreferrer noopener',
+          target: '_blank',
+          className: {
+            keyword: "time-link",
+          },
+          formatHref: {
+            hashtag: (href) => href = "https://www.youtube.com/hashtag/" + href.substring(1),
+            keyword: (keyword) => getSeconds(keyword).toString(),
           },
         }}>
         {sanitizeHTML(replacedContent, {
